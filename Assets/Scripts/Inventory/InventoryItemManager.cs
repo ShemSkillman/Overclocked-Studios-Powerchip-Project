a05677a21059e115e19cea702ec4a4e4b87adea2
Shem Skillman <shem.skillman@students.plymouth.ac.uk>
2021-12-18 15:50:47 +0000

    Correct chip snapping
    Changes: - Chip snaps correctly to the inventory grid.
    Co-Authored-By: James Bridgman <43548013+FearTheRogue@users.noreply.github.com>

--- Assets/Scripts/Inventory/InventoryItemManager.cs
+++ Assets/Scripts/Inventory/InventoryItemManager.cs
@@ -52,21 +52,13 @@ public class InventoryItemManager : MonoBehaviour
         // Parent chip to item manager
         chipRect.transform.parent = transform;
 
-        //Sets chip to slot using central position of chip
-        //chipRect.anchoredPosition = slot.GetComponent<RectTransform>().anchoredPosition;
-
         Vector2 firstChipCellPos = chipUI.GetFirstChipCellPosition(true);
-
-        //X = floorToInt((world_red_dot_anchored_position.x / inventory_grid_width) * slots_in_row)
-
-        //Y = floorToInt((world_red_dot_anchored_position.y / inventory_grid_height) * slots_in_column)
 
         int x = Mathf.FloorToInt((firstChipCellPos.x / 805f) * 5);
         int y = Mathf.FloorToInt(Mathf.Abs((firstChipCellPos.y / 805f) * 5));
 
-
         print("Grid x = " + x + " y = " + y);
-        chipRect.anchoredPosition = GetSlotLocalPosition(x, y);
+        chipRect.anchoredPosition = GetSlotLocalPosition(x, y) - chipUI.GetFirstChipCellPosition(false);
 
         print("Slot pos: " + GetSlotLocalPosition(x,y));
     }
