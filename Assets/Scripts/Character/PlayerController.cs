4e6bd1dad71f6fb2f186f462271f2815ae845a61
Shem Skillman <shem.skillman@students.plymouth.ac.uk>
2022-01-05 09:50:07 +0000

    Hold down attack
    Changes:
     - When attack button is held down sword is constantly swung.
     - Removed delay between attacks.
    -  Removed extra swing after attack button is released.
    
    Co-Authored-By: James Bridgman <43548013+FearTheRogue@users.noreply.github.com>

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     Movement movement;
+    Combat combat;
+    Animator animator;
 
     [SerializeField] VariableJoystick joystick;
+
+    bool isAttacking = false;
+
+    public void ConstantAttack(bool isAttacking)
+    {
+        this.isAttacking = isAttacking;
+
+        if (!isAttacking)
+        {
+            animator.ResetTrigger("MeleeAttack");
+        }
+    }
 
     private void Awake()
     {
         movement = GetComponent<Movement>();
+        combat = GetComponent<Combat>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
         movement.Move(direction);
+
+        if (isAttacking)
+        {
+            combat.StartMeleeAttack();
+        }
     }
 }
