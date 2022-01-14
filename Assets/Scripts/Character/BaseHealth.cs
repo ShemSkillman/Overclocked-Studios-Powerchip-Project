3cb72279084ca036b6349900bc8cac5fdd219183
Foremanski <33032043+Foremanski@users.noreply.github.com>
2022-01-14 22:19:04 +0000

    Added SFX
    - Added hit sounds
    - added death sounds
    - bottomless pit kills enemies

--- Assets/Scripts/Character/BaseHealth.cs
+++ Assets/Scripts/Character/BaseHealth.cs
@@ -2,42 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Audio;
 
 public class BaseHealth : MonoBehaviour
 {
     [SerializeField] protected float hitPoints;
     [SerializeField] private bool destroyOnDeath = true;
+    
+    [SerializeField] private AudioClip hitSound;
     [SerializeField] private AudioClip deathSound;
+    
 
     public UnityAction OnDead, OnHealthChange;
-
+    
     public float Hitpoints { get { return hitPoints; } }
 
     public void TakeDamage(float dmgPoints)
     {
         hitPoints -= dmgPoints;
-        
+
+        //randomise and play hit sound
+        randomiseAudioClip(hitSound);
+
         OnHealthChange?.Invoke();
 
         if (hitPoints <= 0)
         {
             OnDead?.Invoke();
-
-            
+         
             if (destroyOnDeath)
             {              
                 if(gameObject.tag == "Enemy")
                 {
-                    AudioClip clip = gameObject.gameObject.GetComponent<AudioSource>().clip;
-                    Debug.Log("PLAYING");
-                    gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
-                   
+                    AudioSource.PlayClipAtPoint(deathSound, gameObject.transform.position);                                    
                 }
                 Destroy(gameObject);
             }
         }
     }
 
+    private void randomiseAudioClip(AudioClip audio)
+    {
+
+        float pitchVariation = 0.1f;
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+
+        audioSource.clip = audio;
+        audioSource.pitch =  Random.Range(audioSource.pitch - pitchVariation, audioSource.pitch + pitchVariation);
+
+        audioSource.Play();
+    }
+    
     public bool IsDead()
     {
         return hitPoints <= 0;
