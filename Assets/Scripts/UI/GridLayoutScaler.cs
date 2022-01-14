1a190996444416ea1c48d10588c784c97fff9e5f
James Bridgman <43548013+FearTheRogue@users.noreply.github.com>
2022-01-14 20:19:14 +0000

    Screen Scaling Adjustments
    Changes:
    - Adjusted anchor points to scale with screen size correctly
    - Added safe area script for mobile devices
    - Added script to handle inventory cell size based on screen size

---
+++ Assets/Scripts/UI/GridLayoutScaler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class GridLayoutScaler : MonoBehaviour
+{
+    private GridLayoutGroup gridLayout;
+    RectTransform rectTransform;
+
+    private void Start()
+    {
+        gridLayout = GetComponentInChildren<GridLayoutGroup>();
+        rectTransform = GetComponent<RectTransform>();
+
+
+        float cellWidth = rectTransform.rect.width / gridLayout.constraintCount;
+        float cellHeight = rectTransform.rect.height / gridLayout.constraintCount;
+
+        float cellSize = Mathf.Min(cellWidth, cellHeight);
+
+        gridLayout.cellSize = new Vector2(cellSize, cellSize);
+    }
+}
