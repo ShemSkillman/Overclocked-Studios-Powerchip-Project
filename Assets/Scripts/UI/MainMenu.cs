f28f3aebd1e24613478b1ea2cd8054c76a6ef197
Shem Skillman <shem.skillman@students.plymouth.ac.uk>
2022-01-14 23:12:53 +0000

    Main menu level select
    Changes:
     - Changed main menu layout.
     - Added 3 buttons for level select including the tutorial.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject startGameMenu;
+    [SerializeField] GameObject levelSelectMenu;
+
+    public void GoToLevelSelect()
+    {
+        startGameMenu.SetActive(false);
+        levelSelectMenu.SetActive(true);
+    }
+}
