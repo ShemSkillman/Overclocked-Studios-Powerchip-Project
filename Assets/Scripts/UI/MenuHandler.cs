b378151be887e25e3d858ca946bcb6eaaf960ec2
Shem Skillman <shem.skillman@students.plymouth.ac.uk>
2022-01-15 16:07:44 +0000

    Loading screen
    Changes:
     - Added fade out effect and loading screen when selecting a level to load from the main menu.

--- Assets/Scripts/UI/MenuHandler.cs
+++ Assets/Scripts/UI/MenuHandler.cs
@@ -2,11 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
+    [SerializeField] Image background;
+    [SerializeField] Text loadingText;
+    [SerializeField] GameObject levelSelectMenu;
+
+    Coroutine inProgress;
+
     public void StartGame(string sceneName)
     {
+        if(inProgress == null)
+        {
+            inProgress = StartCoroutine(LoadLevel(sceneName));
+        }
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        levelSelectMenu.SetActive(false);
+
+        while (background.color.a <= 1)
+        {
+            Color c = background.color;
+            c.a += 0.01f;
+            background.color = c;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        loadingText.gameObject.SetActive(true);
         SceneManager.LoadScene(sceneName);
     }
 
